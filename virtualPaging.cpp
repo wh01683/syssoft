8cf40cbc2fba3849797e69ba090372a118ce79b1
Trent <trumpet2012@gmail.com>
2014-11-01 14:17:14 -0400

    Added file with rudimentary start of the main function.

---
+++ virtualPaging.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string input;
+    cout << "Enter a string:\n";
+    getline(cin,input);
+    cout << "\nYou entered " <<input << endl;
+}
