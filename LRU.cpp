6cfa1f532e302720903d4f3ca51d1547ab7a5304
Robert Howerton <wh01683@georgiasouthern.edu>
2014-11-01 19:52:18 -0400

    Create LRU.cpp
    LRU class implementing LRUPage.cpp

---
+++ LRU.cpp
@@ -0,0 +1,81 @@
+#include "LRUPage.cpp"
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+
+class LRU{
+
+private:
+long timeOfUse;
+int LRUTableSize;
+
+public:
+
+LRUPage *LRUTable;
+
+//constructor used to create a table of pages specific to
+//the LRU class
+
+
+LRU(int tableSize){
+
+LRUTableSize = tableSize;
+LRUTable = new LRUPage[tableSize];
+
+    for (int i = 0; i < tableSize; i++){
+        LRUTable[i].setAge(std::clock());
+        }
+
+}
+/*
+returns the oldest page in the table
+
+*/
+int getOldest(LRUPage *table){
+
+long minAge = table[0].getAge();
+int increment;
+int oldestIndex;
+LRUPage oldest;
+
+
+        for (increment = 1; increment < LRUTableSize; increment){
+
+            if(LRUTable[increment].getAge() < minAge){
+            oldest = LRUTable[increment];
+            minAge = oldest.getAge();
+            oldestIndex = increment;
+            }
+        }
+
+return oldestIndex;
+
+}
+
+
+bool checkForPage(char page){
+
+int inc;
+
+    for(inc = 0; inc < LRUTableSize; inc ++){
+        if(!LRUTable[inc].isSet()){
+                    cout << "Empty page at " << inc << " adding page " << page << endl;
+                    LRUTable[inc].setName(page);
+                    return false;
+                }
+                if (LRUTable[inc].getName() == page){
+                    cout << "Found page " << page << " at pos " << inc << endl;
+                    return true;
+                }
+            }
+            cout << "Page fault looking for: " << page << endl;
+            LRUTable[getOldest()].setName(page);
+            cout << "Added " << page << " to " << getOldest() << " : " << LRUTable[getOldest()].getName() << endl;
+            LRUTable[getOldest()].setAge(std::clock());
+            return false;
+}
+
+
+
+};
