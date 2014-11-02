bc52875c7a7ab674dd2f846ad3db7661161d0e37
Robert Howerton <wh01683@georgiasouthern.edu>
2014-11-01 20:38:21 -0400

    added test area for LRU algorithm

--- virtualPaging.cpp
+++ virtualPaging.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include "FIFO.cpp"
+#include "LRU.cpp"
+
 using namespace std;
 
 int main(){
@@ -9,9 +11,16 @@ int main(){
 //    getline(cin,input);
 //    cout << "\nYou entered " <<input << endl;
     string test = "abcdaec";
+
+
+    //FIFO test
     FIFO fifo (4);
     string::iterator it;
     for(std::string::size_type i = 0; i < test.size(); ++i) {
         fifo.checkForPage(test[i]);
     }
+    //LRU test
+    LRU lru (4);
+    for(std::string::size_type i = 0; i < test.size(); ++i) {
+        lru.checkForPage(test[i]);}
 }
