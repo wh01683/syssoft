7deaf91e01eb0635b3c5b3818b447b7e6472a7f3
Robert Howerton <wh01683@georgiasouthern.edu>
2014-11-02 11:24:59 -0500

    Added documentation and added page fault counters
    Each algorithm now has its own unique page fault counter for comparison purposes

--- virtualPaging.cpp
+++ virtualPaging.cpp
@@ -3,6 +3,10 @@
 #include "FIFO.cpp"
 #include "LRU.cpp"
 
+/** This is the test area for the page replacement algorithms. Each algorithm has its own page fault counter.
+*/
+
+
 using namespace std;
 
 int main(){
@@ -10,17 +14,31 @@ int main(){
 //    cout << "Enter a string:\n";
 //    getline(cin,input);
 //    cout << "\nYou entered " <<input << endl;
-    string test = "abcdaec";
 
+    string test = "abcdaeabbbeeeeac";
+    int pageFaultCounterLRU = 0;
+    int pageFaultCounterFIFO = 0;
 
-    //FIFO test
+/** First in First Out algorithm test area.
+*/
     FIFO fifo (4);
     string::iterator it;
     for(std::string::size_type i = 0; i < test.size(); ++i) {
         fifo.checkForPage(test[i]);
-    }
-    //LRU test
+        if (fifo.checkForPage(test[i]) == false) pageFaultCounterFIFO ++;
+        }
+cout << pageFaultCounterFIFO << endl;
+
+
+
+/** Least Recently Used algorithm test area.
+*/
     LRU lru (4);
     for(std::string::size_type i = 0; i < test.size(); ++i) {
-        lru.checkForPage(test[i]);}
-}
+        lru.checkForPage(test[i]);
+        if (lru.checkForPage(test[i]) == false) pageFaultCounterLRU ++;
+        }
+cout << pageFaultCounterLRU << endl;
+
+
+}//end main method
