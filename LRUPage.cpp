622f6f6f9a365cb9190b1bf80e3603c8a068f218
Robert Howerton <wh01683@georgiasouthern.edu>
2014-11-02 11:07:28 -0500

    added documentation

--- LRUPage.cpp
+++ LRUPage.cpp
@@ -1,6 +1,6 @@
-#include "Page.cpp"
 #include <cstdio>
 #include <ctime>
+#include "Page.cpp"
 using namespace std;
 std::clock_t start;
 /**
@@ -11,7 +11,8 @@ std::clock_t start;
 * -- func isMarked()    : Check whether the page is marked.
 * -- func mark()        : Function to mark a page as just accessed.
 * -- func unmark()      : Set the marked attribute for the memory page to false.
-*
+* -- func setAge()      : Set the age of the page. Used when the page is accessed.
+* -- func getAge()      : Returns the age of the page. Used when determining oldest page of the array.
 */
 class LRUPage : public Page{
 
@@ -20,21 +21,41 @@ private:
     long age = std::clock();
 
 public:
+/** Constructs new page; marked true by default
+@param none
+@return new LRU page*/
     LRUPage(){
         marked = true;
     }
+
+/** Checks whether page is marked or not
+@param no parameters
+@return boolean whether marked or not*/
     bool isMarked(void){
         return marked;
     }
+
+/** Marks page
+@param no parameters
+@return no return*/
     void mark(void){
         marked=true;
     }
+
+/** Unmarks page
+@param no parameters
+@return no return*/
     void unmark(void){
         marked=false;
     }
-
+/** Sets new age
+@param new age passed in LRU class
+@return no return*/
     void setAge(long newAge){age = newAge;};
 
+/** gets age of the page
+@param no parameters
+@return returns age*/
     long getAge(){return age;};
 
 };
