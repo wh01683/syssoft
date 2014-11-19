0f382e83bf2c711ecd747ffd4f70e59b83709fac
CrazySushiTaxi <jb15064@georgiasouthern.edu>
2014-11-19 12:40:41 -0500

    Optimal
    yay

--- Optimal.cpp
+++ Optimal.cpp
@@ -12,7 +12,7 @@ class OPT {
 
   private:
   int OPTTableSize;
-  int loc = 0; // 0 = loc
+  
   Page * OPTTable;
   
   public:
@@ -22,6 +22,7 @@ class OPT {
   OPT( int size ){
   OPTTableSize = size;
   OPTTable = new Page[size];
+  int loc = 0; // 0 = loc
   }
 
 int Value [10]; //holds value distances for table from current location.
